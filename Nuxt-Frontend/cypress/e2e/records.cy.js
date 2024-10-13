7341eb3d6b002890589a4c026e2987ffe9c2985e
Biniyamseid <Biniyamseid3@gmail.com>
2024-10-14 00:03:45 +0300

    update

--- Nuxt-Frontend/cypress/e2e/records.cy.js
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('http://localhost:3000/');
-    /* ==== End Cypress Studio ==== */
-  })
-})
